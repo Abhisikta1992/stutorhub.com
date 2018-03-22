4440020d3e441115e0f2d40d2fc094cf04b12b87
Subhra K. Das <das.subhra.k@gmail.com>
2018-03-22 19:36:34 +0530

    0.0.1

--- js/init.js
+++ js/init.js
@@ -1,32 +1,3 @@
-/* -- Popular Course */
 $(".box-toggle").on("click", function() {
     $(this).parent(".courses-box").toggleClass("active");
 });
-
-/* Intro Section */
-if($(".intro-section ").length){
-    $(".intro-section ").each(function ()
-    {
-        var $this = $(this);
-        var myVal = $(this).data("value");
-
-        $this.appear(function()
-        {		
-            $(".intro-section .intro-box").addClass("animated fadeInDown");
-        });
-    });
-}
-
-/* Popular Courses Section */
-if($(".popular-courses").length){
-    $(".popular-courses").each(function ()
-    {
-        var $this = $(this);
-        var myVal = $(this).data("value");
-
-        $this.appear(function()
-        {		
-            $(".popular-courses .courses-box").addClass("animated rollIn");
-        });
-    });
-}
